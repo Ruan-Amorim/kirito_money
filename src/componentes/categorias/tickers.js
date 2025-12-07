3c6508680defa1fd3cdf612ffba99e7965e0e707
Ruan Amorim <ruanamorim0708@gmail.com>
2025-12-07 16:27:09 -0300

    tickers.js

--- src/componentes/categorias/tickers.js
+++ src/componentes/categorias/tickers.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { tratarTickersB3 } from "../services/B3_data.js";
+import { tratarTickersB3, TickersB3 } from "../services/B3_data.js";
 
-const CategoriasTickers = () => {
+const CategoriasTickers = () => { 
 
     const [listTickers, setListTickers] = useState([]);
 
@@ -29,40 +29,31 @@ const CategoriasTickers = () => {
 }
 
 export async function clickTicker(symbol) {
-    let tickersPorCategoria = awaitbuscaCategoriaTicker(symbol);
+    let tickersPorCategoria = await buscaCategoriaTicker(symbol);
+    let ul = document.getElementById("listaOpenCategTickers");
 
-    return(
-        <>
-            <section className="containerOpenCategoriaTickers">
-                <ul>
-                    {tickersPorCategoria.length === 0 && <li>Carregando...</li>} {/* opcional */}
+    ul.innerHTML = ""; // limpa a lista
+    ul.style.position = "absolute";
 
-                    {tickersPorCategoria.map(ticker => (
-                        <li key={ticker.code}>{ticker.name} & {ticker.code}</li>
-                    ))}
-                </ul>
-            </section>
-        </>
-    );
+    for (let x = 0; x < tickersPorCategoria.length; x++) {
+        let li = document.createElement("li");
+        li.innerText = `${tickersPorCategoria[x].name} & ${tickersPorCategoria[x].code}`;
+        li.id = tickersPorCategoria[x].code;
+        ul.appendChild(li);
+    } 
 }
+
 
 export async function buscaCategoriaTicker(nome) {
     let listaTickers = await TickersB3();
-    let list = [];
-    
-    for (let x = 0; x < listaTickers.length; x++) {
-        if (nome === listaTickers[x].especPapel) {
-            list.push({
-                "name": listaTickers[x].nomeCurto,
-                "code": listaTickers[x].codNeg,
-                "symbol": nome,
-            });
-        } else {
 
-        }
-        
-    }
-    return(list);
+    return listaTickers
+        .filter(x => x.especPapel === nome)
+        .map(x => ({
+            name: x.nomeCurto,
+            code: x.codNeg,
+            symbol: nome
+        }));
 }
 
 export default CategoriasTickers;
